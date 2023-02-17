22160b4a09b6c020dc40cf3fc34bbd05925b8c8e
Sten Hulsbergen <stenhulsbergen@gmail.com>
2023-02-17 15:16:06 +0100

    oef

--- Labo 2/oef4.py
+++ Labo 2/oef4.py
@@ -4,19 +4,7 @@ while aantalZijden < 3 or aantalZijden > 10:
     if aantalZijden < 3 or aantalZijden > 10:
         print("Geen geldig getal.")
 
-if aantalZijden == 3:
-    print("driehoek")
-elif aantalZijden == 4:
-    print("vierhoek")
-elif aantalZijden == 5:
-    print("vijfhoek")
-elif aantalZijden == 6:
-    print("zeshoek")
-elif aantalZijden == 7:
-    print("zevenhoek")
-elif aantalZijden == 8:
-    print("achthoek")
-elif aantalZijden == 9:
-    print("negenhoek")
-elif aantalZijden == 10:
-    print("tienhoek")
+vormen = ["driehoek", "vierhoek", "vijfhoek",
+          "zeshoek", "zevenhoek", "achthoek", "negenhoek", "tienhoek"]
+
+print(vormen[aantalZijden-3])
--- Labo 2/oef6.py
+++ Labo 2/oef6.py
@@ -1,28 +1,38 @@
 from datetime import datetime, date
+import locale
+
+locale.setlocale(locale.LC_ALL, 'nl_NL')
 
 Y = datetime.now().year
 seizoenen = {
-    'lente': [
-        {'start': date(Y,  3,  20)},
-        {'einde': date(Y,  6,  20)}
-    ],
-    'zomer': [
-        {'start': date(Y,  6,  21)},
-        {'einde': date(Y,  9,  21)}
-    ],
-    'herfts': [
-        {'start': date(Y,  9,  22)},
-        {'einde': date(Y,  12,  20)}
-    ],
-    'winter': [
-        {'start': date(Y,  12,  21)},
-        {'einde': date(Y,  3,  19)}
-    ],
+    'lente': {
+        'start': date(Y,  3,  20),
+        'einde': date(Y,  6,  20)
+    },
+    'zomer': {
+        'start': date(Y,  6,  21),
+        'einde': date(Y,  9,  21)
+    },
+    'herfts': {
+        'start': date(Y,  9,  22),
+        'einde': date(Y,  12,  20)
+    },
+    'winter': {
+        'start': date(Y,  12,  21),
+        'einde': date(Y+1,  3,  19)
+    }
+
 }
 
 maand = input("Geef de maand in: ")
 dag = int(input("Geef de dag van de maand in: "))
 datum = date(Y, datetime.strptime(maand, '%B').month, dag)
-print(seizoenen['lente']['start'])
-# if datum > seizoenen['lente'] and datum < seizoenen['lente']:
-#     print(f"{datum} is in de {seizoenen[0].key}")
+
+if datum > seizoenen['lente']['start'] and datum < seizoenen['lente']['einde']:
+    print(f"{datum} is in de {list(seizoenen.keys())[0]}")
+elif datum > seizoenen['zomer']['start'] and datum < seizoenen['zomer']['einde']:
+    print(f"{datum} is in de {list(seizoenen.keys())[1]}")
+elif datum > seizoenen['herfts']['start'] and datum < seizoenen['herfts']['einde']:
+    print(f"{datum} is in de {list(seizoenen.keys())[2]}")
+elif datum > seizoenen['winter']['start'] and datum < seizoenen['winter']['einde']:
+    print(f"{datum} is in de {list(seizoenen.keys())[3]}")
